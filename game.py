8671a1bb0ee1bebddd08382b9650bab96ff87726
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-06-14 15:20:28 +0200

    Load Images -> Ships

--- game.py
+++ game.py
@@ -4,4 +4,12 @@ import time
 import random
 
 
-name = "Marc"
+
+
+# Load Images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small"))
+
+# Main Layer ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small"))
