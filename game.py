7c62d747a201cc17e94014c80112778264c44e27
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-06-14 15:23:50 +0200

    load laser images

--- game.py
+++ game.py
@@ -7,9 +7,15 @@ import random
 
 
 # Load Images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 
 # Main Layer ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+
+# Load Laser
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
